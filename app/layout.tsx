0027a6ec0fdd311e15592eddac69bf936fc16e56
Zaine <kyawkokozin223@gmail.com>
2024-08-16 16:00:17 +0630

    navbar section done

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import { Metadata } from "next";
 import "./globals.css";
-import Hero from "@/components/main/Hero";
-import StarsCanvas from "@/components/main/Starbackground";
+import StarsCanvas from "@/components/main/StarBackground";
+import Navbar from "@/components/main/Navbar";
 
 export const metadata: Metadata = {
   title : " Space Portfolio",
@@ -18,6 +18,7 @@ export default function RootLayout({
     <html lang="en">
       <body className="bg-[#030014] overflow-y-scroll overflow-x-hidden">
         <StarsCanvas/>
+        <Navbar/>
         {children}
       </body>
     </html>
